336b4786647020db5bccdcdb0bcfe125553f668f
Morooka <91590634+TSmorooka@users.noreply.github.com>
2021-10-01 21:47:35 +0900

    first commit

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -12,8 +12,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    
+        helloWorldLavel.text = english
+        changeLanguageButton.setTitle("言語変更", for: .normal)
+        
     }
-
-
+    
+    @IBOutlet weak var helloWorldLavel: UILabel!
+    @IBOutlet weak var changeLanguageButton: UIButton!
+    let english = "Hello World"
+    let Japanese = "皆さん　こんにちは"
+    
+    
+    @IBAction func changeLanguage(_ sender: Any) {
+        if helloWorldLavel.text == english{
+            helloWorldLavel.text = Japanese
+        } else{
+            helloWorldLavel.text = english
+        }
+    }
+    
 }
 
